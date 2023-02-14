940b9079a73099ed6b2a0fe8f90bd1c1ac0629fc
unknown <mayoush89k@gmail.com>
2023-02-15 00:24:12 +0200

    Updates

---
+++ precache-manifest.d4d8cee48a70747c81c4c069e09ea667.js
@@ -0,0 +1,338 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a3e7574614bc1e4e9a0",
+    "url": "/world-wikipedia/static/css/main.c419e5c0.chunk.css"
+  },
+  {
+    "revision": "6a3e7574614bc1e4e9a0",
+    "url": "/world-wikipedia/static/js/main.bce331a9.chunk.js"
+  },
+  {
+    "revision": "90f3ea9de9934beddafe",
+    "url": "/world-wikipedia/static/js/runtime~main.6aeb451b.js"
+  },
+  {
+    "revision": "9df498bed3aea0bc4928",
+    "url": "/world-wikipedia/static/css/2.f50def93.chunk.css"
+  },
+  {
+    "revision": "9df498bed3aea0bc4928",
+    "url": "/world-wikipedia/static/js/2.c834ce72.chunk.js"
+  },
+  {
+    "revision": "6f22da4ee1558e6148aa",
+    "url": "/world-wikipedia/static/js/3.5a1c438e.chunk.js"
+  },
+  {
+    "revision": "a108ac1ff9e45836d8ccd5b142ca5d42",
+    "url": "/world-wikipedia/static/media/flags.a108ac1f.gif"
+  },
+  {
+    "revision": "ded2ef6b061b1633e9f17dac5ca43d15",
+    "url": "/world-wikipedia/static/media/city.ded2ef6b.gif"
+  },
+  {
+    "revision": "6b280b36e46f6639b461d91abc383979",
+    "url": "/world-wikipedia/static/media/continents.6b280b36.gif"
+  },
+  {
+    "revision": "4510804f8d50cca8257bd191dff4a4c3",
+    "url": "/world-wikipedia/static/media/capital.4510804f.gif"
+  },
+  {
+    "revision": "d6113f5a2c1897f8ebdee92c6f401614",
+    "url": "/world-wikipedia/static/media/arrow.d6113f5a.gif"
+  },
+  {
+    "revision": "0745fd66cdd30ffb2f718907fae3d40e",
+    "url": "/world-wikipedia/static/media/antigua_and_barbuda.0745fd66.gif"
+  },
+  {
+    "revision": "8a40f62eadc052d92641ec1f32f67053",
+    "url": "/world-wikipedia/static/media/argentina.8a40f62e.gif"
+  },
+  {
+    "revision": "3b8eacddcab97f1af164c1fdaadf661c",
+    "url": "/world-wikipedia/static/media/arrowHover.3b8eacdd.gif"
+  },
+  {
+    "revision": "f8cf7fbff0ef32a45f234b10b3a90a29",
+    "url": "/world-wikipedia/static/media/belize.f8cf7fbf.gif"
+  },
+  {
+    "revision": "2913a9c0ab2fd5b5547699c1e40a67bf",
+    "url": "/world-wikipedia/static/media/bhutan.2913a9c0.gif"
+  },
+  {
+    "revision": "f17be0da237f0b51756e58ee220d5dc5",
+    "url": "/world-wikipedia/static/media/costa_rica.f17be0da.gif"
+  },
+  {
+    "revision": "71f63a770f98413e1961af8df87e5049",
+    "url": "/world-wikipedia/static/media/cyprus.71f63a77.gif"
+  },
+  {
+    "revision": "dfe3ebf95312f40d49bf94b1bf55646a",
+    "url": "/world-wikipedia/static/media/dominica.dfe3ebf9.gif"
+  },
+  {
+    "revision": "d94464285c07ee2db3baccad9a046762",
+    "url": "/world-wikipedia/static/media/ecuador.d9446428.gif"
+  },
+  {
+    "revision": "7ffcd9be072929936cfe6d0e6c27bed9",
+    "url": "/world-wikipedia/static/media/el_salvador.7ffcd9be.gif"
+  },
+  {
+    "revision": "c642e39d8cbc1fbac723b52aae59af66",
+    "url": "/world-wikipedia/static/media/equatorial_guinea.c642e39d.gif"
+  },
+  {
+    "revision": "869cacda6d6d534da758a762ed592ee2",
+    "url": "/world-wikipedia/static/media/eritrea.869cacda.gif"
+  },
+  {
+    "revision": "58cd6801b77f9f03a5a3caf43fd8ced0",
+    "url": "/world-wikipedia/static/media/ethiopia.58cd6801.gif"
+  },
+  {
+    "revision": "2595e2b86f1f307d0231e023615f976c",
+    "url": "/world-wikipedia/static/media/falkland_islands.2595e2b8.gif"
+  },
+  {
+    "revision": "6692e7e68656249287d2f88e2c743df5",
+    "url": "/world-wikipedia/static/media/fiji.6692e7e6.gif"
+  },
+  {
+    "revision": "487339e0a3ba36892bd0b7f089cba1e8",
+    "url": "/world-wikipedia/static/media/georgia.487339e0.gif"
+  },
+  {
+    "revision": "f03cf62dd0bfdba3d8e074e64b70aa56",
+    "url": "/world-wikipedia/static/media/grenada.f03cf62d.gif"
+  },
+  {
+    "revision": "6b5b84647f17b2b38ee306b615afffc7",
+    "url": "/world-wikipedia/static/media/guam.6b5b8464.gif"
+  },
+  {
+    "revision": "7483d37327b87618617f16be318642f7",
+    "url": "/world-wikipedia/static/media/guatemala.7483d373.gif"
+  },
+  {
+    "revision": "fb4222443cf341eaeddc627f340f3f54",
+    "url": "/world-wikipedia/static/media/guyana.fb422244.gif"
+  },
+  {
+    "revision": "728a75d180e87433f0888511b3491b91",
+    "url": "/world-wikipedia/static/media/israel.728a75d1.gif"
+  },
+  {
+    "revision": "fd847aa19a1c6dc19b87200064969012",
+    "url": "/world-wikipedia/static/media/jamaica.fd847aa1.gif"
+  },
+  {
+    "revision": "b5232085c63354a5925ef5964014ae7b",
+    "url": "/world-wikipedia/static/media/kazakhstan.b5232085.gif"
+  },
+  {
+    "revision": "992c6f8931be36c362d05f7e3b4ffe43",
+    "url": "/world-wikipedia/static/media/kenya.992c6f89.gif"
+  },
+  {
+    "revision": "a4fb87ea2b0723a28abe649c70aaa091",
+    "url": "/world-wikipedia/static/media/kiribati.a4fb87ea.gif"
+  },
+  {
+    "revision": "bcbbe57936e9a35bf02dd0e438315a2c",
+    "url": "/world-wikipedia/static/media/kyrgyzstan.bcbbe579.gif"
+  },
+  {
+    "revision": "1a2eb7fd2a142bc8132b976e0843acfc",
+    "url": "/world-wikipedia/static/media/lesotho.1a2eb7fd.gif"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/world-wikipedia/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b2af2223d98f8966d9409b411c1299c8",
+    "url": "/world-wikipedia/static/media/malaysia.b2af2223.gif"
+  },
+  {
+    "revision": "d4dfbf51e39600d349c029b9d4513df7",
+    "url": "/world-wikipedia/static/media/martinique.d4dfbf51.gif"
+  },
+  {
+    "revision": "7b36a8c4712921287245b2ae334ff467",
+    "url": "/world-wikipedia/static/media/mexica.7b36a8c4.gif"
+  },
+  {
+    "revision": "7b36a8c4712921287245b2ae334ff467",
+    "url": "/world-wikipedia/static/media/mexico.7b36a8c4.gif"
+  },
+  {
+    "revision": "e08f52ff179ee3de1c8a53351a8fd4a2",
+    "url": "/world-wikipedia/static/media/moldova.e08f52ff.gif"
+  },
+  {
+    "revision": "9c9a81e1115de4fd8e74684a4500dcc0",
+    "url": "/world-wikipedia/static/media/mongolia.9c9a81e1.gif"
+  },
+  {
+    "revision": "e7cc1e720ad8f0f38c9c84799c805265",
+    "url": "/world-wikipedia/static/media/montenegro.e7cc1e72.gif"
+  },
+  {
+    "revision": "64bd6f2d044f2eefbaca405d7a4aefa8",
+    "url": "/world-wikipedia/static/media/montserrat.64bd6f2d.gif"
+  },
+  {
+    "revision": "83939414406c3b46de1b5c4d55da6079",
+    "url": "/world-wikipedia/static/media/myanmar.83939414.gif"
+  },
+  {
+    "revision": "358fd141177d33929302b0ab30d80691",
+    "url": "/world-wikipedia/static/media/namibia.358fd141.gif"
+  },
+  {
+    "revision": "0a53ef2069f8d63585495a16a2737fe4",
+    "url": "/world-wikipedia/static/media/nepal.0a53ef20.gif"
+  },
+  {
+    "revision": "6bc663bab72ce1ea092a7b48c84a3b52",
+    "url": "/world-wikipedia/static/media/new_caledonia.6bc663ba.gif"
+  },
+  {
+    "revision": "3ee939f35ecd351993394654029b04c2",
+    "url": "/world-wikipedia/static/media/niue.3ee939f3.gif"
+  },
+  {
+    "revision": "32eb931a72453ef04e65abe9b8d0d8b8",
+    "url": "/world-wikipedia/static/media/north_korea.32eb931a.gif"
+  },
+  {
+    "revision": "b77b3dc697756e3047dc809316616a19",
+    "url": "/world-wikipedia/static/media/papua_new_guinea.b77b3dc6.gif"
+  },
+  {
+    "revision": "0aac196904a99f01159465125ec7aebe",
+    "url": "/world-wikipedia/static/media/peru.0aac1969.gif"
+  },
+  {
+    "revision": "d2f4a9ce744ed4a24b2fad3b4f928d5e",
+    "url": "/world-wikipedia/static/media/pitcairn_islands.d2f4a9ce.gif"
+  },
+  {
+    "revision": "054dde47035acb6e009d9dba4cfe5af0",
+    "url": "/world-wikipedia/static/media/portugal.054dde47.gif"
+  },
+  {
+    "revision": "4839f8559f8f1bc23a30cd6609ad4459",
+    "url": "/world-wikipedia/static/media/puerto_rico.4839f855.gif"
+  },
+  {
+    "revision": "8172905062c53aef1845493efb0d5e80",
+    "url": "/world-wikipedia/static/media/saint_kitts_and_nevis.81729050.gif"
+  },
+  {
+    "revision": "7108e77b08a5f8773adf9aaf75665040",
+    "url": "/world-wikipedia/static/media/saudi_arabia.7108e77b.gif"
+  },
+  {
+    "revision": "0feb5b70e7da77992be9082e6da08bd1",
+    "url": "/world-wikipedia/static/media/scotland.0feb5b70.gif"
+  },
+  {
+    "revision": "bde113139a245bab9e78facb0aac4848",
+    "url": "/world-wikipedia/static/media/serbia.bde11313.gif"
+  },
+  {
+    "revision": "0b71302479f06450cb1f5e6ae30bdf61",
+    "url": "/world-wikipedia/static/media/singapore.0b713024.gif"
+  },
+  {
+    "revision": "02b2dcb6998d089b99562574e6960e65",
+    "url": "/world-wikipedia/static/media/solomon_islands.02b2dcb6.gif"
+  },
+  {
+    "revision": "6de0f5cf6fbf9bb449d723245fabca6c",
+    "url": "/world-wikipedia/static/media/south_africa.6de0f5cf.gif"
+  },
+  {
+    "revision": "3e9e703c9bbaf74843cdd729bedb3bce",
+    "url": "/world-wikipedia/static/media/south_korea.3e9e703c.gif"
+  },
+  {
+    "revision": "a76cbc3762f1c31fcf691c60a4e005fb",
+    "url": "/world-wikipedia/static/media/south_sudan.a76cbc37.gif"
+  },
+  {
+    "revision": "ae9702af74c257c5d4197fc57a74f53d",
+    "url": "/world-wikipedia/static/media/spain.ae9702af.gif"
+  },
+  {
+    "revision": "f50127bef82797a28875bd6e99cc3ece",
+    "url": "/world-wikipedia/static/media/sri_lanka.f50127be.gif"
+  },
+  {
+    "revision": "8172905062c53aef1845493efb0d5e80",
+    "url": "/world-wikipedia/static/media/st_kitts_and_nevis.81729050.gif"
+  },
+  {
+    "revision": "890994d75046329785ccd71bc5e8580b",
+    "url": "/world-wikipedia/static/media/swaziland.890994d7.gif"
+  },
+  {
+    "revision": "c47b5e6616c7989d22cfdcf17bd10c37",
+    "url": "/world-wikipedia/static/media/taiwan.c47b5e66.gif"
+  },
+  {
+    "revision": "78a6d8d9c795125e92d94cc75ae725f9",
+    "url": "/world-wikipedia/static/media/turkmenistan.78a6d8d9.gif"
+  },
+  {
+    "revision": "ea6930cea3f30c5b79a80083e590f8c7",
+    "url": "/world-wikipedia/static/media/tuvalu.ea6930ce.gif"
+  },
+  {
+    "revision": "52369ca33277b24b4ea240343dc779fb",
+    "url": "/world-wikipedia/static/media/united_kingdom.52369ca3.gif"
+  },
+  {
+    "revision": "ec8570cb9447aad9e208faa059707933",
+    "url": "/world-wikipedia/static/media/united_states.ec8570cb.gif"
+  },
+  {
+    "revision": "afea34f418e028f268b7fed8987d04a1",
+    "url": "/world-wikipedia/static/media/us_virgin_islands.afea34f4.gif"
+  },
+  {
+    "revision": "4ed3a2eb095031ee7665966fa2b7f5d2",
+    "url": "/world-wikipedia/static/media/vanuatu.4ed3a2eb.gif"
+  },
+  {
+    "revision": "1330563b9becc5500c4457326db63270",
+    "url": "/world-wikipedia/static/media/vatican_city.1330563b.gif"
+  },
+  {
+    "revision": "365b5bf80e235ca04ea33b63652daa2f",
+    "url": "/world-wikipedia/static/media/wales.365b5bf8.gif"
+  },
+  {
+    "revision": "5d160fc8aa7007ecd8b034744687153a",
+    "url": "/world-wikipedia/static/media/westren_sahara.5d160fc8.gif"
+  },
+  {
+    "revision": "adbf4901dd29aa462662e109f6894e6e",
+    "url": "/world-wikipedia/static/media/wiki-Continents.adbf4901.gif"
+  },
+  {
+    "revision": "75e2dd0d33450fd2720dab5bac90ac04",
+    "url": "/world-wikipedia/static/media/zimbabwe.75e2dd0d.gif"
+  },
+  {
+    "revision": "2b2b60d9c8ede3546d109618d92e541c",
+    "url": "/world-wikipedia/index.html"
+  }
+];
